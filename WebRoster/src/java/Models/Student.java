9b8e8835d636ed208932c2771c9e7bfebd71d02e
AndrewHankewycz <ahankewycz@gmail.com>
2015-02-26 22:52:01 -0500

    Adding roster files.

--- WebRoster/src/java/Models/Student.java
+++ WebRoster/src/java/Models/Student.java
@@ -1,15 +1,15 @@
 package Models;
 
 public class Student {
-    private String firstName;
-    private String lastName;
-    private String id;
-    private int teamId;
+    private String firstname;
+    private String lastname;
+    private String psuid;
+    private int team;
     
     public Student(String fName, String lName, String id, int teamId){
-        this.firstName = fName;
-        this.lastName = lName;
-        this.id = id;
-        this.teamId = teamId;
+        this.firstname = fName;
+        this.lastname = lName;
+        this.psuid = id;
+        this.team = teamId;
     }
 }

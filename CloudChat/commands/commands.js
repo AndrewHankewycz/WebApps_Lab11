10b110e9a713570943c8c491d5fb9695eeee8ca9
austinkelleher <austin.kell47@gmail.com>
2015-05-02 19:35:33 -0400

    Add create room command.

--- CloudChat/commands/commands.js
+++ CloudChat/commands/commands.js
@@ -6,6 +6,10 @@ var request = require('request'),
  * Command name mapped to the command function
  */
 module.exports = {
+  /**
+   * Command for joining a room
+   * ex: /join pokemon
+   */
   'join' : function(io, socket, args) {
     request.post(config.DAO_URL, {
         form: {
@@ -15,7 +19,24 @@ module.exports = {
       },
       function (error, response, body) {
           if (!error && response.statusCode == 200 && body != "-1") {
-              RoomHelper.addUserToRoom(parseInt(body))
+              RoomHelper.addUserToRoom(parseInt(body));
+          }
+      });
+  },
+  /**
+   * Command for creating a room
+   * ex: /create pokemon
+   */
+  'create' : function(io, socket, args) {
+    request.post(config.DAO_URL, {
+        form: {
+          action: 'createRoom',
+          topic: args
+        }
+      },
+      function (error, response, body) {
+          if (!error && response.statusCode == 200 && body != "-1") {
+              RoomHelper.addUserToRoom(parseInt(body));
           }
       });
   }

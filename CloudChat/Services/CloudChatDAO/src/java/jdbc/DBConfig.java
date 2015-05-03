ae545c384e8f081c9fa91d3e1774ddd2da5e2597
AndrewHankewycz <ahankewycz@gmail.com>
2015-05-03 15:08:01 -0400

    Moved JAVA code to CloudChatServer folder of root

--- CloudChat/Services/CloudChatDAO/src/java/jdbc/DBConfig.java
+++
@@ -1,8 +0,0 @@
-package jdbc;
-
-public class DBConfig {
-    protected static final String DB_NAME = "CloudChatDB";
-    protected static final String DB_HOST = "jdbc:derby://localhost:1527/" + DB_NAME;
-    protected static final String DB_USER = "webapps";
-    protected static final String DB_PASS = "wearepennstate";
-}

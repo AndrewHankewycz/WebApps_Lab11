46f1ea8a4d933246a64553656c07f8e5a9269056
AndrewHankewycz <ahankewycz@gmail.com>
2015-05-06 12:22:03 -0400

    Fixing bug with sending messages after joining a new room.

--- CloudChat/js/chat.js
+++ CloudChat/js/chat.js
@@ -8,6 +8,8 @@ function setChatUsers(room) {
 function sendMessage() {
   var message = $("#messageBox").val();
 
+  console.log(message + " " + roomIdViewing);
+
   if(message === '')
     return;
 
@@ -18,3 +20,47 @@ function sendMessage() {
 
   $("#messageBox").val("");
 }
+
+socket.on('message', function(data) {
+  if(data.roomId === roomIdViewing) {
+    $("#messages").append("<li>" + data.username + ": " + data.message + "</li>");
+  }
+
+  //Keep track of the messages the user should see
+  //regardless of whether they're currently viewing
+  //the room that this message is being sent to. That
+  //way when they switch to this room, they will immediately
+  //be shown these messages without having to poll the server
+  for(var i = 0; i < rooms.length; i++) {
+    if(rooms[i].id === data.roomId) {
+      rooms[i].messages.push({
+        userId: data.userId,
+        username: data.username,
+        message: data.message
+      });
+    }
+  }
+});
+
+socket.on('join', function(data) {
+  for(var i = 0; i < rooms.length; i++) {
+    if(rooms[i].id === data.roomId) {
+      var username = data.username;
+
+      rooms[i].users.push({
+        userId: data.userId,
+        username: username
+      });
+
+      $("#users").append("<p>" + username + "</p>");
+      break;
+    }
+  }
+});
+
+socket.on('me_joined', function(data) {
+  roomIdViewing = data.roomData.id;
+  $("#messages").html("");
+  $("#users").html("");
+  setChatUsers(data.roomData);
+});
